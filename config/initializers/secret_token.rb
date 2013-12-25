5ddf7dffbbdcd0405655d9670ecff91140ea69d9
jarvis1 <eldoni95@gmail.com>
2013-12-25 15:47:36 -0700

    rails new, create Contacts scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4LiveStreaming::Application.config.secret_key_base = 'b37e17481bd43ce3f42f97473e63fa1816ffba5bb60de23752d36134bf3de88c4215517f6b7b63f4dd60aba943feee8a79782b3286a4e37a6680ad719e901557'
